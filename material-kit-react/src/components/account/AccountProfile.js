5ccd267eadfbe4d34d282bc801d42cffc1451f9c
meriambs <bensalah.meriem0@gmail.com>
2021-05-09 03:12:59 +0100

    parite user et profile

--- material-kit-react/src/components/account/AccountProfile.js
+++ material-kit-react/src/components/account/AccountProfile.js
@@ -1,3 +1,82 @@
+// import moment from 'moment';
+// import {
+//   Avatar,
+//   Box,
+//   Button,
+//   Card,
+//   CardActions,
+//   CardContent,
+//   Divider,
+//   Typography
+// } from '@material-ui/core';
+// import  {useSelector} from 'react-redux';
+
+// const user = {
+//   avatar: '/static/images/avatars/avatar_6.png',
+//   city: 'Los Angeles',
+//   country: 'USA',
+//   jobTitle: 'Senior Developer',
+//   name: 'Katarina Smith',
+//   timezone: 'GTM-7'
+// };
+
+// const AccountProfile = ({ ...rest}) => {
+//   const user=useSelector((state =>state.user));
+
+//   <Card  {...rest}>
+//     <CardContent>
+//       <Box
+//         sx={{
+//           alignItems: 'center',
+//           display: 'flex',
+//           flexDirection: 'column'
+//         }}
+//       >
+//         <Avatar
+//           src={user.avatar}
+//           sx={{
+//             height: 100,
+//             width: 100
+//           }}
+//         />
+//         <Typography
+//           color="textPrimary"
+//           gutterBottom
+//           variant="h3"
+//         >
+//           {user.name}
+//         </Typography>
+//         <Typography
+//           color="textSecondary"
+//           variant="body1"
+//         >
+//           {`${user.city} ${user.country}`}
+//         </Typography>
+//         <Typography
+//           color="textSecondary"
+//           variant="body1"
+//         >
+//         {`${moment().format('hh:mm A')} `}
+//         </Typography>
+//       </Box>
+//     </CardContent>
+//     <Divider />
+//     <CardActions>
+//       <Button
+//         color="primary"
+//         fullWidth
+//         variant="text"
+//       >
+//         Upload picture
+//       </Button>
+//     </CardActions>
+//   </Card>
+//         };
+
+// export default AccountProfile;
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import moment from 'moment';
 import {
   Avatar,
@@ -7,67 +86,71 @@ import {
   CardActions,
   CardContent,
   Divider,
-  Typography
+  Typography,
+  makeStyles
 } from '@material-ui/core';
 
-const user = {
-  avatar: '/static/images/avatars/avatar_6.png',
-  city: 'Los Angeles',
-  country: 'USA',
-  jobTitle: 'Senior Developer',
-  name: 'Katarina Smith',
-  timezone: 'GTM-7'
+import  {useSelector} from 'react-redux';
+
+
+const useStyles = makeStyles(() => ({
+  root: {},
+  avatar: {
+    height: 100,
+    width: 100
+  }
+}));
+
+const AccountProfile = ({ className, ...rest }) => {
+  const classes = useStyles();
+
+  const user=useSelector((state =>state.user))
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardContent>
+        <Box
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+        >
+          <Avatar
+            className={classes.avatar}
+            src={`http://localhost:3003/${user.photo}`}
+          />
+          <Typography
+            color="textPrimary"
+            gutterBottom
+            variant="h3"
+          >
+            {user.name}
+          </Typography>
+         
+          <Typography
+            className={classes.dateText}
+            color="textSecondary"
+            variant="body1"
+          >
+            {`${moment().format('hh:mm A')} `}
+          </Typography>
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button
+          color="primary"
+          fullWidth
+          variant="text"
+        >
+         
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
-const AccountProfile = (props) => (
-  <Card {...props}>
-    <CardContent>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
-        <Avatar
-          src={user.avatar}
-          sx={{
-            height: 100,
-            width: 100
-          }}
-        />
-        <Typography
-          color="textPrimary"
-          gutterBottom
-          variant="h3"
-        >
-          {user.name}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body1"
-        >
-          {`${user.city} ${user.country}`}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body1"
-        >
-          {`${moment().format('hh:mm A')} ${user.timezone}`}
-        </Typography>
-      </Box>
-    </CardContent>
-    <Divider />
-    <CardActions>
-      <Button
-        color="primary"
-        fullWidth
-        variant="text"
-      >
-        Upload picture
-      </Button>
-    </CardActions>
-  </Card>
-);
+
 
 export default AccountProfile;
--- material-kit-react/src/index.js
+++ material-kit-react/src/index.js
@@ -2,10 +2,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from "./redux/store";
+
 
 ReactDOM.render((
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+    </Provider>
   </BrowserRouter>
 ), document.getElementById('root'));
 
